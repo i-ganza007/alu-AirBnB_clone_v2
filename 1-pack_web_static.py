70e74d34980fe6a10fffe3a0f4eee3100b24fe9a
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-06-10 10:46:03 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""Comment"""
-from fabric.api import local
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
 from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    """Comment again"""
-    local("mkdir -p versions")
+    """
+    making an archive on web_static folder
+    """
 
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(timestamp)
-
-    result = local("tar -cvzf {} web_static".format(archive_path))
-
-    if result.failed:
-        return None
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
     else:
-        return archive_path
+        return None
